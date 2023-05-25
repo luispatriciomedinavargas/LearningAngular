63668eb5a8e9d6e9b59285364122ef4cd025f7c9
patricio <p.medina.vargas@accenture.com>
2023-05-25 14:39:55 -0300

    added three buttons, add +1 to our counter,another just -1, and the last one makes a reset of the value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+// this is our heart/brain/principal component
+// the other exist to most easly to work because you have independent files to have ONLY ONE THING separate beetween them
+// (app.component.html/spect.ts/module.ts/)
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+//property
+  public title:string = 'Hola mundo';
+  public counter:number = 19;
+
+
+// here is the method that you have in app.component.html, and angular knows it by reference because in the app.module.ts you have all reference there
+// and well also is the only method calls like its
+  increaseBy(value:number):void {
+  this.counter +=value;
+ }
+ resetCounter():void{
+  this.counter=10;
+ }
+
 }
