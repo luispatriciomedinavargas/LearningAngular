0fc24ccf67e7b127d9a5722b06ccfcc287b2d757
patricio <p.medina.vargas@accenture.com>
2023-05-25 18:42:22 -0300

    how is work ngif

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
+  public name: string = "ironman";
+  public age: number = 45;
 
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero(): void {
+    this.name = "Batman"
+  }
+
+  changeAge(): void {
+    this.age = 50
+  }
+
+  resetForm():void{
+    this.name="iroman";
+    this.age=45;
+  }
 }
